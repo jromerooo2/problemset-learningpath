e50523d44790e0781c339027f9396bde56ea7501
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2021-12-25 16:58:10 -0600

    pascal's triangle JS

---
+++ numRow.js
@@ -0,0 +1,19 @@
+var generate = function (numRows) {
+    const triangleArray = [];
+  
+    for (let i = 0; i < numRows; i++) {
+      const row = [];
+  
+      for (let j = 0; j <= i; j++) {
+        const last = triangleArray[i - 1] || [];
+        const sum = last[j - 1] + last[j];
+        row.push(sum || 1);
+      }
+  
+      triangleArray.push(row);
+    }
+  
+    return triangleArray;
+  };
+
+console.log(generate(3));
