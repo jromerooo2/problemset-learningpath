dc65fb68e16966de0bede3050156bd8b66c4684e
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2021-12-27 14:36:57 -0600

    numRow js

--- numRow.js
+++ numRow.js
@@ -1,9 +1,9 @@
 var generate = function (numRows) {
     const triangleArray = [];
-  
+
     for (let i = 0; i < numRows; i++) {
       const row = [];
-  
+    
       for (let j = 0; j <= i; j++) {
         const last = triangleArray[i - 1] || [];
         const sum = last[j - 1] + last[j];
